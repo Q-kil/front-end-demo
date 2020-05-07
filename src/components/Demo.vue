828070e007de5da584b1dab75fa8347b7c7d0578
KilFront <kaifawebb@gmail.com>
2020-05-07 21:36:31 +0800

    add_animation

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="demo">
+    <div class="ball">
+      <h4>小球来回运动</h4>
+      <p>一个小球从向右匀速移动 200px，然后移动回来，再移动过去，最后停留在 200px 处。</p>
+      <div class=ball-box>
+        <div></div>
+      </div>
+    </div>
     <div class="mi-logo">
       <h4>小米官网logo</h4>
       <div class="logo-box">
@@ -22,6 +29,17 @@ export default {
 <style lang="scss">
 .demo {
   text-align: center;
+  .ball {
+    .ball-box {
+      div {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background: #0ff;
+        animation: move 2s linear 3 alternate both;
+      }
+    }
+  }
   .mi-logo {
     .logo-box {
       text-align: left;
@@ -59,6 +77,14 @@ export default {
         transform: translate3d(55px, 0, 0);
       }
     }
+  }
+}
+@keyframes move {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(200px, 0);
   }
 }
 </style>
